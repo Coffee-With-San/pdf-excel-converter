d77d947d4b9ec519aa04ec8b1762a8858d436aa4
San103 <palbanjessan15@gmail.com>
2024-02-29 11:23:30 +0800

    wip

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -9,9 +9,35 @@ class FileUploadController extends Controller
 {
     public function upload(Request $request)
     {
-        // dd($request->all());
+
+        $head = collect([
+            'CtrlNo',
+            'ORSDate',
+            'ORS#',
+            'Office',
+            'Payee',
+            'Account Name',
+            'Account Code',
+            'Amount Remarks'
+        ]);
         $in = new Parser();
         $re = $in->parseFile($request->file);
-        dd($re->getText());
+        $transfrom = explode("\n", $re->getText());
+        $record = collect($transfrom);
+
+        // $header = $record->first(function ($val, $key) {
+        //     return strpos($val, 'CtrlNoORSDate') === 0;
+        // });
+        // $parts = explode("\t", $header);
+        // $firstPart = str_replace(" ", "\t", $parts[0]);
+        // $result = preg_replace('/(CtrlNo)(ORSDate)/', "$1\t$2", $firstPart);
+        // $output = $result . "\t" . implode("\t", array_slice($parts, 1));
+        $rec = $record->search(function (string $item, int $key) {
+            return strpos($item, 'CtrlNoORSDate') === 0;
+        });
+        // $rec = $record->skipUntil(function (int $item, int $key) {
+        //     return strpos($item, 'CtrlNoORSDate');
+        // });
+        dd($record->slice($rec + 1));
     }
 }
--- resources/js/Pages/FileUpload.tsx
+++ resources/js/Pages/FileUpload.tsx
@@ -1,5 +1,5 @@
-import React, { ChangeEvent, useState } from "react";
-import { useForm } from "@inertiajs/react";
+import React, { ChangeEvent, useEffect } from "react";
+import { useForm, router } from "@inertiajs/react";
 
 const FileUpload = () => {
     const { data, setData, post } = useForm({
@@ -11,6 +11,7 @@ const FileUpload = () => {
 
         if (selectedFile) {
             setData("file", selectedFile);
+            router.post(route("file.upload"), { file: selectedFile });
         }
     };
     const uploadHandle = (e) => {
@@ -44,13 +45,13 @@ const FileUpload = () => {
                 </div>
             </div>
             <div>
-                <button
+                {/* <button
                     onClick={uploadHandle}
                     type="button"
                     className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
                 >
                     Convert
-                </button>
+                </button> */}
             </div>
         </div>
     );
